f1c9e3fb20ad33a6d41d268dc1e6466d45ba3a8a
Wenwen1477 <163106315+Wenwen1477@users.noreply.github.com>
2025-03-17 21:17:46 +0700

    V8

---
+++ Assets/using UnityEngine; using UnityEngine.AI;  public class EnemyAI  MonoBehaviour {     public Transform player;  ตั้งค่าให้เป็นตัวละครผู้เล่น     private NavMeshAgent agent;      void Start()     {.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class usingUnityEngine;usingUnityEngine.AI;publicclassEnemyAIMonoBehaviour{publicTransformplayer;ตั้งค่าให้เป็นตัวละครผู้เล่นprivateNavMeshAgentagent;voidStart(){ : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
